8c1d1e355343426dcb77a7f81f27e3870262f122
Shadman Ahmed <shadman.ahmed098@gmail.com>
2022-02-23 11:51:04 +0000

    basic main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+# Terraform init will download any required packages
+
+
+# define cloud povider
+provider "aws"{
+    
+# define region
+    region = "eu-west-1"
+}
+
+
+# define what we want to launch
