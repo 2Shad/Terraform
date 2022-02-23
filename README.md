4f8a71a3da66d12426e6d54cae37bebb63667203
Shadman Ahmed <shadman.ahmed098@gmail.com>
2022-02-23 14:02:17 +0000

    Added ssh keys to main.tf

--- README.md
+++ README.md
@@ -22,3 +22,25 @@
 - Windows search `env` and open **Edit the system Environment variable**
 - Select **Environment Variables**
 - Under **User variables for $User** select **New** and add the 2 variables
+
+### Set-up ssh keys
+Generate ssh keys by `ssh-keygen -t rsa -b 2048`.
+
+#### To add the keys to terraform add `key_name= "aws_key"` to the `"aws_instance"` resource, in the same resource also add a `connection` section:
+```
+connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = file("/home/user/.ssh/aws_key")
+      timeout     = "4m"
+   }
+```
+
+#### You also need to add another `aws_key_pair` resource in the file:
+```
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws_key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDbvRN/gvQBhFe+dE8p3Q865T/xTKgjqTjj56p1IIKbq8SDyOybE8ia0rMPcBLAKds+wjePIYpTtRxT9UsUbZJTgF+SGSG2dC6+ohCQpi6F3xM7ryL9fy3BNCT5aPrwbR862jcOIfv7R1xVfH8OS0WZa8DpVy5kTeutsuH5suehdngba4KhYLTzIdhM7UKJvNoUMRBaxAqIAThqH9Vt/iR1WpXgazoPw6dyPssa7ye6tUPRipmPTZukfpxcPlsqytXWlXm7R89xAY9OXkdPPVsrQdkdfhnY8aFb9XaZP8cm7EOVRdxMsA1DyWMVZOTjhBwCHfEIGoePAS3jFMqQjGWQd user@pc"
+}
+```
--- main.tf
+++ main.tf
@@ -10,3 +10,37 @@ provider "aws"{
 
 
 # define what we want to launch
+# Automate the process of creating EC2 instance
+
+# name of the resource
+resource "aws_instance" "shadman_tf_app" {
+
+    # which AMI to use 
+    ami = "ami-07d8796a2b0f8d29c"
+
+    # define instance type
+    instance_type = "t2.micro"
+
+    # define ssh key
+    key_name= "aws_tf"
+
+    # Enable public IP?
+    associate_public_ip_address = true
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = file("/config/.ssh/aws_tf")
+      timeout     = "4m"
+    }
+
+    # Name of the instance
+    tags = {
+        Name = "eng103a_shadman_tf_app"
+    }
+}
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws_tf"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+C8B1ZgjSWqGtcaVwsa7Bo720MQR6XCCbMqCA9DMeJPbDz4eEqRRsdS3v8/CXlurWPZSjhe7nOigLzuq2xLqpApfTbZrCLbHmGw8I7iZnrpfho7i9KNOZG/qIKymLMC5Y+zLNDGzQBK8RYeeceE1uf0jN0YzDxoeWXYkyddlcEvvjI+oPTpmw2F/4RXwqIVr3BVu0igo7ZI99NggWzgVswH2ndog+iihXsLOMX2/5tWrVNU4GUlknmEAdehtOmKf5tN8w/SbHxAEbaEnayrqncbqNs6qmhr1sNafHBZVuVWfLaC8AUwLrlRSDhnT7G9M/2TFfFY9NZLaRf5Ig5OqH abc@0f0541c92239"
+}
